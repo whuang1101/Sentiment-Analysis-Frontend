70dcf826aaedf6fbf89849bcf3467a5f7907b210
Wilson Huang <wilsonhuang11012000@gmail.com>
2024-06-15 19:01:59 -0400

    Created a neutral category

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import "./App.css"
 import Pie from './Pie';
+import ReactLoading from 'react-loading';
 function App() {
   const [data, setData] = useState("hey");
   const [newData, setNewData] = useState("no")
   const [loading, setLoading] = useState(true)
+  const[newLoading, setNewLoading] = useState(false)
   const [currentURL, setCurrentURL] = useState("")
   const [pieData, setPieData] = useState("")
+  const [badLink, setBadLink] = useState(false)
 
     const sendData = async () => {
+        setNewLoading(true)
         try {
             const response = await fetch(`http://127.0.0.1:5000/posts/${data}`, {
                 method: 'GET',
@@ -21,30 +25,51 @@ function App() {
 
             if (response.ok) {
                 const responseData = await response.json();
+                setBadLink(false)
                 setNewData(responseData)
                 setLoading(false)
                 setCurrentURL(data)
-                setPieData([["Sentiment", "Number of Comments"],["Positive Comments",responseData.positive.length], ["Negative Comments", responseData.negative.length]])
-                console.log(responseData);
+                setPieData([["Sentiment", "Number of Comments"],["Positive Comments",responseData.positive.length], ["Negative Comments", responseData.negative.length],["Neutral Comments", responseData.neutral.length]])
+                console.log(responseData)
             } else {
                 console.error('Error:', response.statusText);
+                setBadLink(true)
+
             }
         } catch (error) {
             console.error('Error:', error);
+
+        }
+        finally {
+          setNewLoading(false);
         }
     };
   return (
     <div className='border'>
       <div className="middle-container">
+    <div className="header-container">
+
+
     <h1>Sentiment Analysis</h1>
     <label htmlFor="">Please enter a reddit post url!</label>
     <input type="text" value={data} onChange={(e) =>setData(e.target.value)}/>
     <button onClick={sendData}> Send data</button>
+    {newLoading &&    
+    <div className='loading'>
+       <ReactLoading type={'spinningBubbles'} color={"white"} height={'30%'} width={'30%'} />
+    </div>}{
+      badLink &&
+    <div className='bad-link'>
+        URL does not follow the pattern "https://www.reddit.com/r/[subreddit]/comments/"
+    </div>}
+    </div>
     
     {!loading && 
       <>
-      
-        <p>For more details go to <a href={currentURL}> currentURL</a></p>
+        <h2>Currently looking at the sentiments of comments of "{newData.title}"</h2>
+        <p>For more details go to <a href={currentURL}> {currentURL}</a></p>
+        <Pie data={pieData}/>
+
         <div className="three-columns">
           {[1, 2, 3].map((index) => (
             <div key ={index} >
@@ -53,7 +78,7 @@ function App() {
                 <div className='comments'>
                   <h1>Positive Comments</h1>
                   {newData.positive.map((positiveItem, positiveIndex) => (
-                    <div key={`positive-${positiveIndex}`} className='comment'>{positiveItem.content}</div>
+                    <p key={`positive-${positiveIndex}`} className='comment'>{positiveItem.content}</p>
                   ))}
                 </div>
               </div>
@@ -63,25 +88,24 @@ function App() {
                   
                   <h1>Negative Comments</h1>
                   {newData.negative.map((negativeItem, negativeIndex) => (
-                    <div key={`negative-${negativeIndex}`} className='comment'>{negativeItem.content}</div>
+                    <p key={`negative-${negativeIndex}`} className='comment'>{negativeItem.content}</p>
                   ))}
                 </div>
               </div>
             ) : index === 3 && newData.neutral.length !== 0 && (
               <div key ={index} className='three-comments' >
-
                 <div className='comments'>
+                  
                   <h1>Neutral Comments</h1>
                   {newData.neutral.map((neutralItem, neutralIndex) => (
-                    <div key={`neutral-${neutralIndex}`} className='comment'>{neutralItem.content}</div>
+                    <p key={`neutral-${neutralIndex}`} className='comment'>{neutralItem.content}</p>
                   ))}
                 </div>
-              </div>
+            </div>
             )}
               </div>
           ))}
         </div>
-        <Pie data={pieData}/>
 
       </>
     }
